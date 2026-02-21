755ccdf325cb800a7940cfdc018888089bbd1800
AdityaGopalakrishnan <adityagjayanthi@gmail.com>
2026-02-21 09:11:10 +0530

    Update talker.cpp

--- src/talker.cpp
+++ src/talker.cpp
@@ -8,41 +8,39 @@
 
 using namespace std::chrono_literals;
 
-class MinimalPublisher : public rclcpp::Node
+class Talker : public rclcpp::Node
 {
 public:
-  MinimalPublisher()
-  : Node("talker"), count_(0)
-  {
-    this->declare_parameter<std::string>("message_prefix", "Hello");
+    Talker()
+        : Node("talker"), count_(0)
+    {
+        // Declare parameter with default value
+        this->declare_parameter("message_prefix", "Hello");
 
-    publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
-
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-  }
+        publisher_ = this->create_publisher<std_msgs::msg::String>("/chatter", 10);
+        timer_ = this->create_wall_timer(
+            1000ms, std::bind(&Talker::timer_callback, this));
+    }
 
 private:
-  void timer_callback()
-  {
-    std::string prefix = this->get_parameter("message_prefix").as_string();
+    void timer_callback()
+    {
+        std::string prefix = this->get_parameter("message_prefix").as_string();
+        auto message = std_msgs::msg::String();
+        message.data = prefix + ": " + std::to_string(count_++);
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        publisher_->publish(message);
+    }
 
-    auto message = std_msgs::msg::String();
-    message.data = prefix + ": " + std::to_string(count_++);
-    
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
-  }
-
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Talker>());
+    rclcpp::shutdown();
+    return 0;
 }
